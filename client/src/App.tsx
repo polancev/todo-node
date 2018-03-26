aec55940f477bf8fdb35ebc412891f9e4fd56270
polancev <lummicom@gmail.com>
2018-03-26 23:47:59 +0400

    axios add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
+import axios from 'axios';
 import './App.css';
 
 const logo = require('./logo.svg');
 
 import { CategoryI } from './types/Category';
+import { TodoI } from './types/Todo';
 
 interface State {
   categories: Array<CategoryI>;
+  todos: Array<TodoI>;
 }
 
 interface Props {
@@ -16,24 +19,38 @@ class App extends React.Component {
   state: State;
   constructor(props: Props) {
     super(props);
-    this.state = { categories: new Array<CategoryI>()};
+    this.state = { 
+      categories: [],
+      todos: []
+    };
   }
 
-  handler = () => {
-    fetch('http://localhost:7777/category')
-    .then(res => res.json())
-    .then(res => this.setState({ categories: res })); 
+  getCategories = () => {
+    axios
+      .get('http://localhost:7777/category')
+      .then(res => res.data)
+      .then(categories => this.setState({ categories })); 
+  }
+
+  getTodos = () => {
+    axios
+      .get('http://localhost:7777/todo')
+      .then(res => res.data)
+      .then(todos => this.setState({ todos })); 
   }
 
   render() {
-    const { categories } = this.state;
+    const { categories, todos } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-header" onClick={this.getTodos}>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro" onClick={this.handler}>
+        <ul>
+          {todos.map((i: TodoI) => <li key={i.name}>{i.name}</li>)}
+        </ul>
+        <p className="App-intro" onClick={this.getCategories}>
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
         <ul>
